c8206ceb13d0cb87fea17096564add77ea89d6c5
Fekinox <williamowenfowlk@gmail.com>
2018-03-15 00:07:06 -0400

    Lesson 2 added

---
+++ Lesson 2/Label.gd
@@ -0,0 +1,15 @@
+extends Label
+
+# Just as a little hint...
+# Define a signal using
+
+# signal signal_name
+
+# and call it like so:
+	
+# emit_signal("signal_name")
+
+# thank me later...
+
+func _ready():
+	pass
